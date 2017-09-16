e6c58739262f30c0c2ad86369fbbe741994ec5e5
sfmajors373 <sfmajors373@gmail.com>
2017-09-16 12:42:08 -0400

    More fixing of the line reading loop, so I am back to where I was this morning with the same error

--- microarchitecture.py
+++ microarchitecture.py
@@ -53,105 +53,121 @@ def executeInstructions(Data):
     with open(args.filename, 'r') as f:
         line = f.readline()
         print(line)
-        # if the line is not commented out
-        if isCommented(line) == True:
-            line = f.readline()
-        elif isCommented(line) == False:
-            line = line.strip()
-            temp = line.split(" ")
-            # extract the 8 digits I need
-            binaryStr = temp[0]
-            print(binaryStr)
-            # determine what that 8 digits means to me
-            if len(instructions) == 0:
-                instructions.append(dict[binaryStr])
-                print(dict[binaryStr])
-                print(instructions)
-            # if there is exactly one instruction in the list
-            elif len(instructions) ==1:
-                if instructions[0] == 'initialize':
-                    initialize(registers)
-                    instructions.pop()
-                    print('registers initialized')
+        while line != "":
+            # if the line is not commented out
+            if isCommented(line) == True:
+                line = f.readline()
+            elif isCommented(line) == False:
+                line = line.strip()
+                temp = line.split(" ")
+                # extract the 8 digits I need
+                binaryStr = temp[0]
+                print(binaryStr)
+                # determine what that 8 digits means to me
+                if len(instructions) == 0:
+                    instructions.append(dict[binaryStr])
+                    print(dict[binaryStr])
                     print(instructions)
-                elif instructions[0] == 'set register':
-                    reg = binToDec(binaryStr)
-                    instructions.append(reg)
-                    print(instructions)
-                else:
-                    print('we have a problem with instruction[0]')
-                    print(instructions)
-                    # break
-            # if there are exactly 2 instruction in list
-            elif len(instructions) == 2:
-                instructions.append(dict[binaryStr])
-                print(instructions)
-            # if there are exactly 3 instructions in list
-            elif len(instructions) == 3:
-                if instruction[2] == 'save':
-                    instructions.append(binToDec(binaryStr))
-                    print(instructions)
-                elif instruction[2] == 'multiply':
-                    instructions.append(binToDec(binaryStr))
-                    print(instructions)
-                elif instruction[2] == 'print':
-                    if instructions[1] == 0:
-                        print(registers[reg0])
-                        # break
-                    elif instructions[1] == 1:
-                        print(registers[reg1])
-                        # break
-                    elif instructions[1] == 2:
-                        print(registers[reg2])
-                        # break
-                    elif instructions[1] == 3:
-                        print(registers[reg3])
-                        instructions.clear()
-                        # break
+                    line = f.readline()
+                # if there is exactly one instruction in the list
+                elif len(instructions) ==1:
+                    if instructions[0] == 'initialize':
+                        initialize(registers)
+                        instructions.pop()
+                        print('registers initialized')
+                        print(instructions)
+                        line = f.readline()
+                    elif instructions[0] == 'set register':
+                        reg = binToDec(binaryStr)
+                        instructions.append(reg)
+                        print(instructions)
+                        line = f.readline()
                     else:
-                        print('error with finding register for instructions[1]')
-                else:
-                    print('error with instruction[2]')
-            # if instructions has exactly 4 instructions in list
-            elif len(instructions) == 4:
-                if instructions[2] == 'save':
-                    if instructions[1] == 0:
-                        registers[reg0] = binToDec(binaryStr)
-                        instructions.clear()
-                    elif instructions[1] == 1:
-                        registers[reg1] = binToDec(binaryStr)
-                        instructions.clear()
-                    elif instructions[1] == 2:
-                        registers[reg2] = binToDec(binaryStr)
-                        instructions.clear()
-                    elif instructions[1] == 3:
-                        registers[reg3] = binToDec(binaryStr)
-                        instructions.clear()
+                        print('we have a problem with instruction[0]')
+                        print(instructions)
+                        # break
+                # if there are exactly 2 instruction in list
+                elif len(instructions) == 2:
+                    instructions.append(dict[binaryStr])
+                    print(instructions)
+                    line = f.readline()
+                # if there are exactly 3 instructions in list
+                elif len(instructions) == 3:
+                    if instruction[2] == 'save':
+                        instructions.append(binToDec(binaryStr))
+                        print(instructions)
+                        line = f.readline()
+                    elif instruction[2] == 'multiply':
+                        instructions.append(binToDec(binaryStr))
+                        print(instructions)
+                        line = f.readline()
+                    elif instruction[2] == 'print':
+                        if instructions[1] == 0:
+                            print(registers[reg0])
+                            # break
+                        elif instructions[1] == 1:
+                            print(registers[reg1])
+                            # break
+                        elif instructions[1] == 2:
+                            print(registers[reg2])
+                            # break
+                        elif instructions[1] == 3:
+                            print(registers[reg3])
+                            instructions.clear()
+                            # break
+                        else:
+                            print('error with finding register for instructions[1]')
                     else:
-                        print('register in instructions[1] not found to save to')
-                elif instructions[2] == 'multiply':
-                    instructions.append(binToDec(binaryStr))
-                else:
-                    print('error instructions[2] not anticiipated with instructions length 4')
-            # if instructions has exactly 5 instructions
-            if len(instructions) == 5:
-                if instructions[2] == 'multiply':
-                    if instructions[1] == 0:
-                        registers[reg0] = instructions[4] * binToDec(binaryStr)
-                        instructions.clear()
-                    elif instructions[1] == 1:
-                        registers[reg1] = instructions[4] * binToDec(binaryStr)
-                        instructions.clear()
-                    elif instructions[1] == 2:
-                        registers[reg2] = instructions[4] * binToDec(binaryStr)
-                        instructions.clear()
-                    elif instructions[1] == 3:
-                        registers[reg3] = instructions[4] * binToDec(binaryStr)
-                        instructions.clear()
+                        print('error with instruction[2]')
+                # if instructions has exactly 4 instructions in list
+                elif len(instructions) == 4:
+                    if instructions[2] == 'save':
+                        if instructions[1] == 0:
+                            registers[reg0] = binToDec(binaryStr)
+                            instructions.clear()
+                            line = f.readline()
+                        elif instructions[1] == 1:
+                            registers[reg1] = binToDec(binaryStr)
+                            instructions.clear()
+                            line = f.readline()
+                        elif instructions[1] == 2:
+                            registers[reg2] = binToDec(binaryStr)
+                            instructions.clear()
+                            line = f.readline()
+                        elif instructions[1] == 3:
+                            registers[reg3] = binToDec(binaryStr)
+                            instructions.clear()
+                            line = f.readline()
+                        else:
+                            print('register in instructions[1] not found to save to')
+                    elif instructions[2] == 'multiply':
+                        instructions.append(binToDec(binaryStr))
+                        line = f.readline()
                     else:
-                        print('register in instructions[1] not found to save to')
-                else:
-                    print('the instructions list is too long to not be multiplying')
+                        print('error instructions[2] not anticiipated with instructions length 4')
+                # if instructions has exactly 5 instructions
+                if len(instructions) == 5:
+                    if instructions[2] == 'multiply':
+                        if instructions[1] == 0:
+                            registers[reg0] = instructions[4] * binToDec(binaryStr)
+                            instructions.clear()
+                            line = f.readline()
+                        elif instructions[1] == 1:
+                            registers[reg1] = instructions[4] * binToDec(binaryStr)
+                            instructions.clear()
+                            line = f.readline()
+                        elif instructions[1] == 2:
+                            registers[reg2] = instructions[4] * binToDec(binaryStr)
+                            instructions.clear()
+                            line = f.readline()
+                        elif instructions[1] == 3:
+                            registers[reg3] = instructions[4] * binToDec(binaryStr)
+                            instructions.clear()
+                            line = f.readline()
+                        else:
+                            print('register in instructions[1] not found to save to')
+                    else:
+                        print('the instructions list is too long to not be multiplying')
 
             # else:
             #     print('the length of instructions is not accounted for')
